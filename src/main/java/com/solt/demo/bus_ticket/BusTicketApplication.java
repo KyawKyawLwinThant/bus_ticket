aa185e242066d4358dd4e22e44d919d22db1802d
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2019-09-14 10:02:00 +0630

    first

---
+++ src/main/java/com/solt/demo/bus_ticket/BusTicketApplication.java
@@ -0,0 +1,13 @@
+package com.solt.demo.bus_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BusTicketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/solt/demo/bus_ticket/BusTicketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solt.demo.bus_ticket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusTicketApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
